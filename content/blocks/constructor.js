70584135cc310dede82b48b8a359cf7aea742a6d
Lynohara <jonatangabriel1004@gmail.com>
2023-03-04 18:28:14 -0300

    constructor

---
+++ content/blocks/constructor.js
@@ -0,0 +1,8 @@
+// Constructs blocks up to 2x2 in size. Ideal for Payload Mass Drivers with Copper Deconstructors
+// maxBlockSize: 2
+
+const lib = require("blib");
+const copConstructor = extend(Constructor, "Copper Constructor", {
+    size:3,
+    health: 600
+});
